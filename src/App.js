46932a02aee646622fd1f97406a53235724b9130
dannypellatt <danielpellatt92@gmail.com>
2023-05-30 13:28:19 -0700

    changed hard coded

--- src/App.js
+++ src/App.js
@@ -3,8 +3,47 @@ import './App.css';
 import ContactCard from './ContactCard';
 
 const App = () => {
+  const contacts = [
+    {
+      avatarUrl: "https://via.placeholder.com/150",
+      name: "Jenny Han",
+      email: "notreasl@notreasl.com",
+      age: 25
+    },
+    {
+      avatarUrl: "https://via.placeholder.com/150",
+      name: "Jenny bloo",
+      email: "notreasgsl@notreasl.com",
+      age: 35
+    },
+    {
+      avatarUrl: "https://via.placeholder.com/150",
+      name: "Jim Him",
+      email: "not@notreasl.com",
+      age: 99
+    },
+    {
+      avatarUrl: "https://via.placeholder.com/150",
+      name: "Per Son",
+      email: "hiemail@notreasl.com",
+      age: 206
+    }
+  ];
   return(
-    <ContactCard/>
+    <div>
+      {contacts.map((contact, index) => {
+        return(
+          <ContactCard 
+            key={index}
+            avatarUrl = {contact.avatarUrl}
+            name = {contact.name}
+            email = {contact.email}
+            age = {contact.age}
+          />
+        )
+      })}
+    </div>
+    
   )
 }
 
--- src/ContactCard.js
+++ src/ContactCard.js
@@ -1,17 +1,19 @@
 import React, { useState } from 'react';
 
-const ContactCard = () => {
+const ContactCard = (props) => {
   const [showAge, setShowAge] = useState(false);
-    return (
-      <div className="contact-card">
-        <img src="https://via.placeholder.com/150" alt="profile"></img>
-        <div className="user-details">
-          <p>Name: John Smith</p>
-          <p>Email: johnsmith@reallyreallyrealemail.com</p>
-          {showAge ? <p>Age: 25</p> : null}
-        </div>
+
+  return (
+    <div className="contact-card">
+      <img src={props.avatarUrl} alt="profile"></img>
+      <div className="user-details">
+        <p>Name: {props.name}</p>
+        <p>Email: {props.email}</p>
+        <button onClick={()=> setShowAge(!showAge)}>Show Age</button>
+        {showAge ? <p>Age: {props.age}</p> : null}
       </div>
-    )
+    </div>
+  )
 }
 
 export default ContactCard;
